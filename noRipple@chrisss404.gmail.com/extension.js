d93cf74a7e3480c0d16b52462a26e1e56d8a9b66
Christian Hofer <chrisss404@gmail.com>
2013-07-01 12:26:46 +0200

    updated to work with gnome-shell 3.8

--- noRipple@chrisss404.gmail.com/extension.js
+++ noRipple@chrisss404.gmail.com/extension.js
@@ -7,9 +7,12 @@ function init(extensionMeta) {
 function enable() {
     Layout.HotCorner.prototype.rippleAnimationBackup = Layout.HotCorner.prototype.rippleAnimation;
     Layout.HotCorner.prototype.rippleAnimation = function() {};
+    Layout.HotCorner.prototype._rippleAnimationBackup = Layout.HotCorner.prototype._rippleAnimation;
+    Layout.HotCorner.prototype._rippleAnimation = function() {};
 }
 
 function disable() {
     Layout.HotCorner.prototype.rippleAnimation = Layout.HotCorner.prototype.rippleAnimationBackup;
+    Layout.HotCorner.prototype._rippleAnimation = Layout.HotCorner.prototype._rippleAnimationBackup;
 }
 
