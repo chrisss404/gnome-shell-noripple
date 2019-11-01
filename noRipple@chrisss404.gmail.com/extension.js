4d80cd21e7fab7d4224aaba391596e6038ba3a1a
Christian Hofer <chrisss404@gmail.com>
2019-11-01 16:23:21 +0100

    support gnome-shell 3.34

--- noRipple@chrisss404.gmail.com/extension.js
+++ noRipple@chrisss404.gmail.com/extension.js
@@ -1,4 +1,5 @@
 const Layout = imports.ui.layout;
+const Ripples = imports.ui.ripples;
 
 function init(extensionMeta) {
     // do nothing here    
@@ -9,10 +10,13 @@ function enable() {
     Layout.HotCorner.prototype.rippleAnimation = function() {};
     Layout.HotCorner.prototype._rippleAnimationBackup = Layout.HotCorner.prototype._rippleAnimation;
     Layout.HotCorner.prototype._rippleAnimation = function() {};
+    Ripples.Ripples.prototype._animRippleBackup = Ripples.Ripples.prototype._animRipple;
+    Ripples.Ripples.prototype._animRipple = function(ripple, delay, duration, startScale, startOpacity, finalScale) {};
 }
 
 function disable() {
     Layout.HotCorner.prototype.rippleAnimation = Layout.HotCorner.prototype.rippleAnimationBackup;
     Layout.HotCorner.prototype._rippleAnimation = Layout.HotCorner.prototype._rippleAnimationBackup;
+    Ripples.Ripples.prototype._animRipple = Ripples.Ripples.prototype._animRippleBackup;
 }
 
