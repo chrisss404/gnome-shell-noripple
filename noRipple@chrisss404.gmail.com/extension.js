9b350189548fb2328c44b4a56770c5406a11417a
Christian Hofer <chrisss404@gmail.com>
2023-11-26 17:07:31 +0100

    adapt for gnome-shell 45

--- noRipple@chrisss404.gmail.com/extension.js
+++ noRipple@chrisss404.gmail.com/extension.js
@@ -1,22 +1,8 @@
-const Layout = imports.ui.layout;
-const Ripples = imports.ui.ripples;
+export default class NoRippleExtension {
+    enable() {
+    }
 
-function init(extensionMeta) {
-    // do nothing here    
-}
-
-function enable() {
-    Layout.HotCorner.prototype.rippleAnimationBackup = Layout.HotCorner.prototype.rippleAnimation;
-    Layout.HotCorner.prototype.rippleAnimation = function() {};
-    Layout.HotCorner.prototype._rippleAnimationBackup = Layout.HotCorner.prototype._rippleAnimation;
-    Layout.HotCorner.prototype._rippleAnimation = function() {};
-    Ripples.Ripples.prototype._animRippleBackup = Ripples.Ripples.prototype._animRipple;
-    Ripples.Ripples.prototype._animRipple = function(ripple, delay, duration, startScale, startOpacity, finalScale) {};
-}
-
-function disable() {
-    Layout.HotCorner.prototype.rippleAnimation = Layout.HotCorner.prototype.rippleAnimationBackup;
-    Layout.HotCorner.prototype._rippleAnimation = Layout.HotCorner.prototype._rippleAnimationBackup;
-    Ripples.Ripples.prototype._animRipple = Ripples.Ripples.prototype._animRippleBackup;
+    disable() {
+    }
 }
 
